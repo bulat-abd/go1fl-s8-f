2a02535e6ab6331b4590e22ee66705bfbf88aec8
Bulat <babdullatypov@gmail.com>
2025-09-17 21:38:31 +0300

    Handle rows error

--- parcel.go
+++ parcel.go
@@ -63,7 +63,9 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
         res = append(res, p)
     }
 	// заполните срез Parcel данными из таблицы
-
+	if err := rows.Err(); err != nil {
+        return nil, err
+    }
 	return res, err
 }
 
